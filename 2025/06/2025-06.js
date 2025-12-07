7205d82e47c0e0728ed1dc48eccae0ddc477f951
aikhelis <alex.ikhelis@gmail.com>
2025-12-07 17:41:49 +0000

    2025-06 part2

--- 2025/06/2025-06.js
+++ 2025/06/2025-06.js
@@ -1,52 +1,59 @@
+const { parse } = require('path');
 const {readFileLines} = require('../lib/readFile');
-const testInput    = readFileLines('test.txt');
-const puzzleInput  = readFileLines('input.txt');
+const testInput     = readFileLines('test.txt');
+const puzzleInput   = readFileLines('input.txt');
 const puzzleInput2  = readFileLines('input2.txt');
 
-const input = testInput;
+const input = puzzleInput2;
 console.log('Input:', input);
 
-function solve_part1(input) {
+console.log("PART 1");
+let [problems, operators] = parseProblemsPart1(input);
+console.log('Problems:', problems);
+console.log('Operators:', operators);
+console.log('Result:', evalProblems(problems, operators));
+
+console.log();
+console.log("PART 2");
+[problems, operators] = parseProblemsPart2(input);
+console.log('Problems:', problems);
+console.log('Operators:', operators);
+console.log('Result:', evalProblems(problems, operators));
+
+/* Helper functions */
+function parseProblemsPart1(input) {
     const lines = input.map(line => line.trim().split(/\s+/));
     const operators = lines.pop();
     let problems = transpose(lines);
+    console.log('Lines:', lines);
+    return [problems, operators];
+}
 
-    console.log('Part1 Problems:', problems);
-    console.log('Part1 Operators:', operators);
+function parseProblemsPart2(input) {
+    const lines = input.map(line => line.split(''));
+    const operators = lines.pop().map(o => o.trim()).filter(o => o);
+    let numbers = transpose(lines).map(num => num.join('').trim()).concat('');
+    let problems = [], problem = [], index = 0;
+    numbers.forEach(num => {
+        if (num === '') {
+            problems[index] = problem;
+            problem = [];
+            index++;
+        } else 
+            problem.push(num);
+    });
+    console.log('Lines:', lines);
+    console.log('Numbers:', numbers);
+    return [problems, operators];
+}
 
+function evalProblems(problems, operators) {
     const expression = problems.reduce( (finalExpression, problem, i) => 
         finalExpression += "+" + problem.join(operators[i]),
         '');
-
-    console.log('Part1 Expression:', expression);
-    console.log('PART1 RESULT:', eval(expression));
+    console.log('Expression:', expression);
+    return eval(expression);
 }
-
-function solve_part2(input) {
-    const lines = input.map(line => line.split(''));
-    const operators = lines.pop();
-    // operators.fill
-    let problems = transpose(lines).map(num => num.join(''));
-    // problems = problems.map(problem => {
-    //     problem = problem.map(num => num.split(''));
-    //     problem = transpose(problem).flatMap(num => num.join(''))
-    //     return problem;
-    // });
-    console.log('Part2 Lines:', lines);
-    console.log('Part2 Problems:', problems);
-    console.log('Part2 Operators:', operators);
-
-    // const expression = problems.reduce( (finalExpression, problem, i) => 
-    //     finalExpression += "+" + problem.join(operators[i]),
-    //     '');
-
-    // console.log('Part2 Expression:', expression);
-
-    // console.log('PART2 RESULT:', eval(expression));
-}
-
-solve_part2(input);
-/* Helper functions */
 
 function transpose(matrix) {
     return matrix[0].map((col, i) => matrix.map(row => row[i]));
